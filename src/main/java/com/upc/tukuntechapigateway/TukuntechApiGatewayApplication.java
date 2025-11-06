a31b7c2a043d965701a39fc55427b5254ed46ac1
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-06 10:20:40 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/tukuntechapigateway/TukuntechApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukuntechApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukuntechApiGatewayApplication.class, args);
+    }
+
+}
