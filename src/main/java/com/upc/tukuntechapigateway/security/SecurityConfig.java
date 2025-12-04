4f20bf8bd7899d180d36d54758b94a4928301452
oscaredu1226 <oscaredu01@hotmail.com>
2025-12-04 14:30:05 -0500

    chore(config-server): clean up configuration and enable environment-based settings for deployment

--- src/main/java/com/upc/tukuntechapigateway/security/SecurityConfig.java
+++ src/main/java/com/upc/tukuntechapigateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.upc.tukuntechapigateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -9,12 +10,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
     SecurityWebFilterChain filter(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(ServerHttpSecurity.CorsSpec::disable)
                 .authorizeExchange(ex -> ex
-                        .pathMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().permitAll()
                 )
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
